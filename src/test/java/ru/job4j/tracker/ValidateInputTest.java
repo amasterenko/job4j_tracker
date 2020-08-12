26fc27519899b664242511581fea5eee0ea106f8
Andrey Masterenko H <masterenko.as@gmail.com>
2020-08-12 08:00:38 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#331078]: дописаны тесты: правильный ввод, проверка output.

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -17,4 +17,27 @@ public class ValidateInputTest {
         int selected = input.askInt("Enter menu:");
         assertThat(selected, is(1));
     }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenOutputChecked() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        input.askInt("Enter menu:");
+        assertThat(out.toString(), is("Please enter validate data again."
+                                            + System.lineSeparator()));
+    }
 }
